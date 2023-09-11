3b892b043f9aadfd6c37bec1e7ecfc68cae8b4d7
Vadim Cherepanov <xenos26@gmail.com>
2023-09-11 21:12:39 +0700

    test: add wrong test
    for checking lazygit work with git hooks

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,11 @@
 import { test, expect, describe } from "bun:test";
+import exp from "constants";
 
 describe("Demo tests", () => {
   test("2 + 2", () => {
     expect(2 + 2).toBe(4);
+  })
+  test.skip("wrong test", () => {
+    expect(2 + 2).toBe(5);
   })
 })
