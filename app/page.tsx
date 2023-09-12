0c799d79c4664ce2e3bb003f5e010d9cff1a89a0
Vadim Cherepanov <xenos26@gmail.com>
2023-09-12 19:39:49 +0700

    feat: add some changes for testing lint hooks

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <h1 className={styles.color}>Hello my friend</h1>
+    <>
+      <h1 className={styles.color}>Hello my friend</h1>
+      <p>Some text for testing git hook</p>
+    </>
   )
 }
