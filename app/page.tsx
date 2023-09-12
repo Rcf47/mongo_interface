8c6ae8d2f2161c255d6e453adc762c14f7e6bc92
Vadim Cherepanov <xenos26@gmail.com>
2023-09-12 21:15:25 +0700

    style: add formating with prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 export default function Home() {
-  return (
-    <>
-      <h1 className={styles.color}>Hello my friend</h1>
-      <p>Some text for testing git hook</p>
-    </>
-  )
+	return (
+		<>
+			<h1 className={styles.color}>Hello my friend</h1>
+			<p>Some text for testing git hook</p>
+		</>
+	);
 }
